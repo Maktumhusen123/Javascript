7b348fd66f92e426035985b8ae9922900f6f5135
Maktumhusen Challamarad <44393494+Maktumhusen123@users.noreply.github.com>
2025-01-10 14:54:27 +0530

    Add files via upload

---
+++ Fundamentals Part-1/Challenge3/index.js
@@ -0,0 +1,12 @@
+const scoreDolphins = (96+108+89)/3;
+const scoreKoalas = (88+91+110)/3;
+
+if (scoreDolphins > scoreKoalas){
+    console.log("Dolphis wins the trophy")
+}
+else if(scoreKoalas > scoreDolphins){
+    console.log("Koalas wins the trophy")
+}
+else{
+    console.log("Both wins the trophy")
+}
